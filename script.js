af3765537c54eb617b26e9c9f41a178fb3aac20c
Lian016 <kemilly016allan@gmail.com>
2024-06-11 16:34:57 -0300

    01

---
+++ script.js
@@ -0,0 +1,252 @@
+function continuar() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Você está em sua casa, dormindo ( são 03:00 da manhã)  e de repente acorda com um barulho na cozinha de sua casa… </h3>';
+  formation += '<img src="">';
+  formation += '<br>';
+  formation += '<button class="botoes" onclick="Escolha1()">Você continua dormindo</button>';
+  formation += '<button class="botoes" onclick="Escolha2()">Você levanta e vai vê o que é</button>';
+  $(".principal").append(formation);
+}
+
+
+function Escolha1() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3>  Você acordou 06:00 com fome e foi direto para sua cozinha, quando de repente você dar de cara com o corpo do seu irmão, em seu corpo tinha várias marcas de facadas, você grita desesperado delo seus pais e os mesmos veem em sua direção perguntando ,“porque essa gritaria toda!” sua mãe diz, “o que aconteceu pra você está gritando!” seu pai diz , quando finalmente eles olham para o chão e veem seu filho mais velho do qual eles tanto amavam morto, o desespero toma conta da família e então eles chamam a polícia, um detetive diz para  todos irem para casa de algum familiar mas para que ninguém saia da cidade. </h3>';
+  formation += ' <img src="">';
+  formation += '<br>';
+  formation += '<button class="botoes" onclick="Escolha11()">Continuar</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha11() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Continuação das duas -  06:30, o detetive “lope” veio até a casa dos “ Thompsons” que são os parentes de família “Montrise”( é o sobrenome da família principal), e disse que os três agora são os principais suspeitos do assasinato de Mordan. Então Liana, Erik e Nyan foram levados para a delegacia para o interrogatório._________ Lope - E então quem será o primeiro?</h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha111()">Eu vou ser o primeiro</button>';
+  formation += '<button class="botoes" onclick="Escolha112()">Os meus pais podem ir primeiro</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha111() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Você é levado para sala de interrogatório onde te mandam sentar em uma cadeira, e mesmo estando ali sentado, parado na frente de um detetive a sua mente estar longe, tudo em que você consegue pensar é “como ela sabia? “</h3>'
+  formation += '<h3>Lope - Garoto, ô garoto… garoto! </h3>'
+  formation += '<h3>E você finalmente sai do transe e a única coisa que sai da sua boca é “ela sabia “</h3>'
+  formation += '<h3>Lope - O que ela sabia? Quem sabia? </h3>'
+  formation += '<h3>Minha mãe, quando eu chamei ela e ela veio, assim que ela viu o meu irmão no chão ela me mandou ir atrás dele… mas eu não disse se eu vi uma pessoa, ela só sabia que eram ele… como? </h3>'
+  formation += '<h3>Lope - Boa pergunta garoto, boa pergunta…bom o que você estava fazendo na hora do crime?</h3>'
+  formation += '<h3>Antes de ir dormir eu estava conversando com o meu irmão na cozinha, acho que era sobre um novo jogo que tinha lançado. quando era sei lá umas 22:00 eu fui dormir.</h3>'
+  formation += '<h3>Lope - entendi…14 de fevereiro de 1994, 03:30 foi a hora da morte, quem estava com ele a essa hora?</h3>'
+  formation += '<h3>Minha mãe, claro.</h3>'
+  formation += '<h3>Você é interrogado por mais um tempo, e logo foi liberado, agora você só tem que esperar o interrogatório dos seus pais. </h3>'
+   formation += '<h3>Lope - Senhor Nyan certo?, o senhor não estava em casa na hora do crime, onde estava? </h3>'
+   formation += '<h3>Nyan - eu estava na casa do Teo (irmão de Nyan), ele me pediu pra ajudar a consertar a caminhonete vermelha dele que estava quebrada e eu fui, depois disso eu fui comprar um livro para a minha esposa ela gosta dessas coisas até porque  hoje …era pra ser um dia especial pra ela, a loja fica a 20 min da casa do meu irmão,a loja fica na direção oposta da minha casa, pode perguntar para ele se quiser, E mesmo assim como você ousa a dizer que eu mataria meu próprio filho?!</h3>'
+   formation += '<h3>Lope - Em momento nenhum disse que você tinha matado o seu filho,  quem pensou isso foi você. </h3>'
+   formation += '<h3>Logo a senhora Liana foi chamada </h3>'
+   formation += '<h3> Lopes - Aí estou cansado disso, seria mais fácil se alguém confessasse logo, você não acha?, pois bem, O que estava fazendo na hora do crime . </h3>'
+  formation += '<h3>Liana- Eu estava no meu quarto dormindo, quando eu acordei de susto com o meu filho mais novo me chamando e quando eu vou até a cozinha…Eu…eu vejo o Mordan morto naquele chão…eu não tenho palavras para descrever o que eu senti.</h3>'
+  formation += '<h3>Lopes ver que isso não está  o levando a nada e decide chamar mais algumas pessoas para o interrogatório , Teo .</h3>'
+  formation += '<h3>Lopes - Por aqui senhor Thompsons.</h3>'
+  formation += '<h3>Chegando na sala de interrogatório.</h3>'
+  formation += '<h3>Lopes - Me conte onde estava no dia anterior do crime</h3>'
+  formation += '<h3>Teo - Eu passei o dia com o meu irmão consertando a minha caminhonete e depois de um tempo ele saiu pra ir a uma loja de livros para comprar um presente para a esposa dele,acho que ele saiu às 02:10 por aí, e eu fiquei com a minha esposa o tempo todo.</h3>'
+  formation += '<h3> Lopes - E onde está sua esposa ?</h3>'
+  formation += '<h3> Teo - Estar em casa agora, estar passando mal, cólica endente?</h3>'
+
+  
+  formation += '<br>';
+  formation += '<button class="botoes" onclick="Escolha111p2()">Continuar</button>';
+  $(".principal").append(formation);
+}
+function Escolha111p2() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> O detetive fecha o interrogatório,e manda todo mundo ir para a casa, ele  se vê em um beco sem saída, ele sai da delegacia quando dá de cara com uma mulher, ela fala </h3>'
+  formation += '<h3>   Nora- Nossa triste o que aconteceu com o garoto né…</h3>'
+  formation += '<h3>O detetive a reconheceu era a vizinha dos Thompsons, e como quem não quer nada ele começou a conversar com ela, para tentar descobrir mais alguma coisa </h3>'
+  formation += '<h3> Lope -Sim muito triste mesmo, o garoto gostava muito dos tios né, o senhor Teo parecia muito preocupado com ele</h3>'
+  formation += '<h3>…na verdade nem tanto, ele era super grudado com a tia, eles passavam o dia todo juntos sabe, mas ele odiava o tio dele…só não sei o porquê, Mordan ficou tão triste quando a tia desapa…ela foi viajar</h3>'
+  formation += '<h3>…um dia ela simplesmente sumiu…ele diz que ela foi viajar…o estranho é que ela nunca mais voltou,isso já faz uns 2 anos, estranhamente Mordan vivia espiando a casa do Teo e um dia ele simplesmente parou…e depois disso ele …Todos vamos sentir falta dele. Eu estava com a Liana umas 2 horas antes do ocorrido, estávamos falando sobre nossos maridos, e ela comentou que o seu marido adorava, ela foi embora acho que 02:50.</h3>'
+  formation += '<br>';
+  formation += '<button class="botoes" onclick="Escolha111()">Continuar</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha111p3() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3>Depois disso o detetive soube de imediato quem era o assassino.</h3>'
+  formation += '<h3></h3>'
+  formation += '<h3></h3>'
+  formation += '<h3></h3>'
+  formation += '<br>';
+  formation += '<button class="botoes" onclick="Escolha111p2()">Continuar</button>';
+  $(".principal").append(formation);
+ 
+}
+
+function Escolha112() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Você derrotou os zumbis e chamou Max para seu grupo, logo após vocês saíram do laboratório. </h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha1111()">Procurar um meio de Transporte</button>';
+  formation += '<button class="botoes" onclick="Escolha1112()">Ir a pé</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha1112() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> No meio do caminho, vocês encontram uma horda de zumbis. </h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha11121()">Lutar</button>';
+  formation += '<button class="botoes" onclick="Escolha11122()">Fugir</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha11121() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Vocês lutam e derrotam todos. </h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha1111()">Continuar</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha11122() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Vocês fogem desesperadamente e no meio do caminho são cercado por outros zumbis e são infectados. </h3>'
+  formation += '<br>'
+  $(".principal").append(formation);
+}
+
+function Escolha1111() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> No meio do caminho você encontra Jasmim, que está em cima de uma casa. </h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha11111()">Salvar Jasmin</button>';
+  formation += '<button class="botoes" onclick="Escolha11112()">Ignorar</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha11112() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Jasmin se distrai, tropeça e morre. </h3>'
+  formation += '<button class="botoes" onclick="Escolha11111()">Continuar</button>';
+  $(".principal").append(formation);
+}
+
+var formation = ''
+function Escolha11111() {
+  formation += '<br>';
+  formation += '<h3> Logo após você e seu grupo pensam em um plano para criar uma cura para o vírus. No caminho, vocês acham uma horda de zumbis. </h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha111111()">Lutar</button>';
+  formation += '<button class="botoes" onclick="Escolha111112()">Desviar do Caminho</button>';
+  $(".principal").append(formation);
+}
+var formation = ''
+function Escolha111112() {
+  formation += '<br>';
+  formation += '<h3> Vocês tem uma viagem longa, porém sem nenhuma interrupção e enfim conseguem chegar ao laboratório e por fim você conseguiu salvar o mundo de um apocalipse zumbi. </h3>'
+  formation += '<br>'
+  $(".principal").append(formation);
+}
+
+function Escolha111111() {
+  var formation = ''
+  formation += '<br>';
+  formation += '<h3> Vocês conseguem derrotar eles e ir para o laboratório, porém perde uma vida. No laboratório, vocês encontram um pequeno grupo de zumbis que restaram. </h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha1111111()">Lutar</button>';
+  formation += '<button class="botoes" onclick="Escolha1111112()">Tentar ir sem chamar atenção</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha1111111() {
+  var formation = ''
+  formation += '<br>';
+  formation += '<h3> Vocês conseguem derrotar os zumbis e criam a cura do vírus. </h3>'
+  formation += '<br>'
+  $(".principal").append(formation);
+}
+
+function Escolha12() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Com a ajuda do segurança Apolo, você conseguiu sair em segurança. Fora do laboratório, você e Apolo encontram recursos que podem criar a cura do vírus. </h3>'
+  formation += '<br>'
+  formation += '<button class="botoes" onclick="Escolha121()">Pegar as Recompensas</button>';
+  formation += '<button class="botoes" onclick="Escolha122()">Ignorar</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha121() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Você coleta as recompensas, cria a cura do vírus e salva o mundo. </h3>'
+  formation += '<br>'
+  $(".principal").append(formation);
+}
+
+function Escolha122() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Você encontra uma hordas de zumbi, juntos, vocês não conseguem matar todos os zumbis e morrem. </h3>'
+  formation += '<br>'
+  $(".principal").append(formation);
+}
+
+function Escolha2() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Você vai direto para a cozinha quando dar de cara com o seu irmão sangrando no chão e ver uma pessoas saindo de sua casa, mas não dá tempo de ver o rosto das pessoas que saíram, você grita por ajuda e a sua mãe aparece perguntando “porque essa gritaria toda!”, ela olha rapidamente para o chão e ver seu amado filho quase morto no chão, sua mãe grita e manda você ir atrás dele e você vai, soque nao tem mais ninguém lá fora, quando você volta sua mãe está aos prantos no chão e em seu colo está o seu filho mais velho morto, você chama a polícia assim que eles chegam um detetive fala pra que vocês irem para casa de um parente mas que ninguém pode sair da cidade.      ( a casa dos parentes mais próximos fica a 12 min da casa deles)           </h3>';
+  formation += '<img src="">';
+  formation += '<br>';
+  formation += '<button class="botoes" onclick="Escolha21()">Continue</button>';
+  $(".principal").append(formation);
+}
+
+function Escolha21() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Continuação das duas -  06:30, o detetive “lope” veio até a casa dos “ Thompsons” que são os parentes de família “Montrise”( é o sobrenome da família principal), e disse que os três agora são os principais suspeitos do assasinato de Mordan. Então Liana, Erik e Nyan foram levados para a delegacia para o interrogatório._________ Lope - E então quem será o primeiro? </h3>';
+  formation += '<img src="">';
+  formation += '<br>';
+  formation += '<button class="botoes" onclick="Escolha211()">Eu vou ser o primeiro</button>';
+  formation += '<button class="botoes" onclick="Escolha1()">Os meus pais podem ir primeiro</button>';
+  $(".principal").append(formation);
+}
+
+
+function Escolha211() {
+  var formation = '';
+  formation += '<br>';
+  formation += '<h3> Por azar, você acabou sendo cercado por um bando de zumbis e é infectado. </h3>';
+  formation += '<img src="">';
+  formation += '<br>';
+  $(".principal").append(formation);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
